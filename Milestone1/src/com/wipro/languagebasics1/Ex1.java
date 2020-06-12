13300c0a15e383feddd366e5cb994b2557df15a6
pmyv1 <pmyv1@DESKTOP-Q1026N3>
2020-06-12 09:11:45 +0530

    Language basics

---
+++ Milestone1/src/com/wipro/languagebasics1/Ex1.java
@@ -0,0 +1,9 @@
+package com.wipro.languagebasics1;
+
+public class Ex1 {
+public static void main(String[] args) {
+	String a=args[0];
+	String b=args[1];
+	System.out.println(a+" Technologies "+b);
+}
+}
