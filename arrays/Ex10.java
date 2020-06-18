5b7ba4cff12fdc17948625659eef93098322e15e
170031430 <60382086+170031430@users.noreply.github.com>
2020-06-18 08:10:03 +0530

    Arrays

---
+++ arrays/Ex10.java
@@ -0,0 +1,26 @@
+package com.wipro.arrays;
+
+public class Ex10 {
+public static void main(String[] args) {
+	int arr[]= {1,0,1,0,0,1,1};
+	int Even = 0;
+	  int Odd = arr.length-1;
+	  int[] array = new int[arr.length];
+	  
+	  for (int i = 0; i < arr.length; i++) {
+	    if (arr[i] % 2 == 0) {
+	      array[Even] = arr[i];
+	      Even++;
+	    } 
+	    else {
+	      array[Odd] = arr[i];
+	      Odd--;
+	    }
+	  }
+	for(int i=0;i<arr.length;i++)
+	{
+	System.out.print(array[i]+" ");
+	}
+
+}
+}
