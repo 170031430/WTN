bf77214cc343ea3dc4dba4a9a1a82a4ee95afb28
170031430 <60382086+170031430@users.noreply.github.com>
2020-06-18 08:06:45 +0530

    Delete Ex1.java

--- languagebasics/Ex1.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.languagebasics;
-
-public class Ex1 {
-public static void main(String[] args) {
-	String a=args[0];
-	String b=args[1];
-	System.out.println(a+" Technologies "+b);
-}
-}
