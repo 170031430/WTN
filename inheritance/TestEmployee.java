44d202bd33f0741af7e6775098e7b8fb94ac1f23
170031430 <60382086+170031430@users.noreply.github.com>
2020-06-26 08:10:52 +0530

    Inheritance

---
+++ inheritance/TestEmployee.java
@@ -0,0 +1,11 @@
+package com.wipro.inheritance;
+
+public class TestEmployee {
+	public static void main(String[] args) {
+		Person p=new Person("Yamini");
+		Employee e=new Employee("Yam",10000,2021,"mini");
+		System.out.println(p);
+		System.out.println(e);
+	}
+
+}
