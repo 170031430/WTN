ab9d0c368453077021136bf89ba64ac84307b75b
170031430 <60382086+170031430@users.noreply.github.com>
2020-06-26 08:44:29 +0530

    Abstract Classes

---
+++ abstractclass/General.java
@@ -0,0 +1,10 @@
+package com.wipro.abstractclass;
+
+public class General extends Compartment {
+
+	@Override
+	public void notice() {
+		System.out.println("Notice: You're in General");
+	}
+
+}
---
+++ abstractclass/Luggage.java
@@ -0,0 +1,10 @@
+package com.wipro.abstractclass;
+
+public class Luggage extends Compartment {
+
+	@Override
+	public void notice() {
+		System.out.println("Notice: You're in Luggage");
+	}
+
+}
