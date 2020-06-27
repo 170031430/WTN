ae20d9bef304f0958f8d443650bf0271522181c2
170031430 <60382086+170031430@users.noreply.github.com>
2020-06-27 07:28:24 +0530

    Interfaces

---
+++ com.wipro.interfaces/live/Test.java
@@ -0,0 +1,21 @@
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+public class Test {
+    public static void main(String args[]) {
+        Veena v = new Veena();
+        Saxophone s = new Saxophone();
+        Playable pv, ps; /*c. Place the above instances in a variable of type Playable and then call play().*/
+        pv = new Veena();
+        ps = new Saxophone();
+        
+        v.play(); /*a. Create an instance of Veena and call play() method*/
+        s.play(); /*b. Create an instance of Saxophone and call play() method*/
+        
+        pv.play(); /*c. Place the above instances in a variable of type Playable and then call play().*/
+        ps.play();
+        
+    }
+}
