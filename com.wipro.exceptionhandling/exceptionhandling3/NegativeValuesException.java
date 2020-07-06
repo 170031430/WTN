8941e60f058e85a6267f545dca3ce6a005f58aa2
170031430 <60382086+170031430@users.noreply.github.com>
2020-07-06 20:24:22 +0530

    Exception Handling

---
+++ com.wipro.exceptionhandling/exceptionhandling3/NegativeValuesException.java
@@ -0,0 +1,8 @@
+package exceptionhandling3;
+
+public class NegativeValuesException extends Exception {
+	public NegativeValuesException() {
+		super();
+		System.out.println("NegativeValuesException occured");
+	}
+}
---
+++ com.wipro.exceptionhandling/exceptionhandling5/InvalidAgeException.java
@@ -0,0 +1,8 @@
+package exceptionhandling5;
+
+public class InvalidAgeException extends Exception {
+	public InvalidAgeException() {
+		super();
+		System.out.println("Invalid age");
+	}
+}
